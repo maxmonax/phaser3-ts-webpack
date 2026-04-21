df33035d0f1897df130c1fbba7560693aa7fde8a
Max <maxmonax@gmail.com>
2026-04-22 04:12:55 +0500

    OrientationAlert: move img to html

--- game/src/scaling/OrientationAlert.ts
+++ game/src/scaling/OrientationAlert.ts
@@ -3,7 +3,6 @@ import { Params } from '@/data/Params';
 
 const rotClassName = 'rotate-image-container';
 const imgId = 'rotate__image';
-const imgSrc = './assets/images/rotate-phone-icon-white.png';
 
 export class OrientationAlert {
   private static showOrientationAlert() {
@@ -12,20 +11,6 @@ export class OrientationAlert {
     if (!divGame) return;
     divGame.style.display = 'none';
     divRotate.style.display = 'flex';
-
-    // Get references to the image and container
-    if (!document.getElementById(imgId)) {
-      // Create a new Image object
-      // LogMng.debug('Create a new Image object');
-      const img = new Image();
-      img.id = imgId;
-      img.alt = 'Rotate device to landscape orientation';
-      img.src = imgSrc;
-      if (!Config.ORIENTATION.inLandscape) {
-        img.style.rotate = '-90deg';
-      }
-      divRotate.appendChild(img);
-    }
   }
 
   private static hideOrientationAlert() {
@@ -65,5 +50,10 @@ export class OrientationAlert {
     const ww = window.innerWidth;
     const wh = window.innerHeight;
     Params.isLandscape = ww / wh >= ar;
+
+    const img = document.getElementById(imgId) as HTMLImageElement | null;
+    if (img && !Config.ORIENTATION.inLandscape) {
+      img.style.rotate = '-90deg';
+    }
   }
 }
