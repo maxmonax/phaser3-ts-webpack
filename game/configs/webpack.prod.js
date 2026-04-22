0abe013b845979d81d21ea7ad3358fdfc8c0f880
Max <maxmonax@gmail.com>
2026-04-22 05:28:44 +0500

    feat: add webpack-bundle-analyzer via build:analyze script

--- game/configs/webpack.prod.js
+++ game/configs/webpack.prod.js
@@ -3,75 +3,78 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 // paths
 const __base = path.resolve(__dirname, '..');
 
-module.exports = merge(common, {
-  // prod mode
-  mode: 'production',
-  devtool: false,
+module.exports = (env = {}) =>
+  merge(common, {
+    // prod mode
+    mode: 'production',
+    devtool: false,
 
-  // override resolve to use tsconfig.prod.json
-  resolve: {
+    // override resolve to use tsconfig.prod.json
+    resolve: {
+      plugins: [
+        new TsconfigPathsPlugin({
+          baseUrl: __base,
+          configFile: path.join(__base, 'tsconfig.prod.json'),
+        }),
+      ],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+
+    output: {
+      filename: '[name].[contenthash:4].js',
+      path: path.resolve(__base, 'build'),
+      clean: true,
+    },
+
     plugins: [
-      new TsconfigPathsPlugin({
-        baseUrl: __base,
-        configFile: path.join(__base, 'tsconfig.prod.json'),
+      new webpack.DefinePlugin({ __DEV__: JSON.stringify(false) }),
+      new MiniCssExtractPlugin({
+        filename: 'css/main.css',
       }),
+      ...(env.analyze ? [new BundleAnalyzerPlugin()] : []),
     ],
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-
-  output: {
-    filename: '[name].[contenthash:4].js',
-    path: path.resolve(__base, 'build'),
-    clean: true,
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({ __DEV__: JSON.stringify(false) }),
-    new MiniCssExtractPlugin({
-      filename: 'css/main.css',
-    }),
-  ],
-  module: {
-    rules: [
-      // css|sass files
-      {
-        test: /\.(css|scss|sass)$/i,
-        use: [
-          // minificator
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          {
-            loader: 'sass-loader',
-            options: {
-              api: 'modern',
+    module: {
+      rules: [
+        // css|sass files
+        {
+          test: /\.(css|scss|sass)$/i,
+          use: [
+            // minificator
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            {
+              loader: 'sass-loader',
+              options: {
+                api: 'modern',
+              },
             },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: ['...', new CssMinimizerPlugin()],
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          phaser: {
+            test: /[\\/]node_modules[\\/]phaser[\\/]/,
+            name: 'phaser',
+            filename: 'js/phaser.[contenthash:4].js',
+            priority: 20,
+            reuseExistingChunk: true,
           },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: ['...', new CssMinimizerPlugin()],
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        phaser: {
-          test: /[\\/]node_modules[\\/]phaser[\\/]/,
-          name: 'phaser',
-          filename: 'js/phaser.[contenthash:4].js',
-          priority: 20,
-          reuseExistingChunk: true,
         },
       },
     },
-  },
-});
+  });
